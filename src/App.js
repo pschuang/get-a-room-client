e58574c2015961edbdd9597baf0779bdea5e5565
Pin Shan Chuang <pshanchuang@gmail.com>
2022-11-05 20:00:36 +0800

    feat: 新增 routes & 調整 socket 邏輯

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
+import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import BulletinPage from './pages/BulletinPage/BulletinPage'
 import ChatRoom from './pages/ChatRoom/ChatRoom'
+import ChatRoomPage from './pages/ChatRoomPage/ChatRoomPage'
+import QuestionDetailPage from './pages/QuestionDetailPage/QuestionDetailPage'
+import { io } from 'socket.io-client'
+import LoginInput from './components/LoginInput/LoginInput'
 
 // test
 
-function App() {
+const App = () => {
+  const [socket, setSocket] = useState(
+    io('http://localhost:8000', { autoConnect: false })
+  )
+  const [userId, setUserId] = useState('1')
+
   return (
-    <Routes>
-      <Route exact path="/" element={<BulletinPage />} />
-      <Route exact path="/chatroom" element={<ChatRoom />} />
-    </Routes>
+    <>
+      <LoginInput userId={userId} setUserId={setUserId} socket={socket} />
+      <Routes>
+        <Route exact path="/" element={<BulletinPage />} />
+        {/* <Route
+        exact
+        path="/login"
+        element={<BulletinPage setSocket={setSocket} />}
+      /> */}
+        <Route exact path="/chatroom" element={<ChatRoom />} />
+        <Route
+          exact
+          path="/chat/:roomId"
+          element={<ChatRoomPage socket={socket} userId={userId} />}
+        />
+        <Route
+          exact
+          path="/question/:id"
+          element={<QuestionDetailPage socket={socket} />}
+        />
+      </Routes>
+    </>
   )
 }
 
