d752ca5c48710f2689ce5d836fd6e2a9b24df46b
Pin Shan Chuang <pshanchuang@gmail.com>
2022-11-05 11:25:12 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import BulletinPage from './pages/BulletinPage/BulletinPage'
+import ChatRoom from './pages/ChatRoom/ChatRoom'
+
+// test
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Routes>
+      <Route exact path="/" element={<BulletinPage />} />
+      <Route exact path="/chatroom" element={<ChatRoom />} />
+    </Routes>
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/BulletinPage/BulletinPage.js
@@ -0,0 +1,5 @@
+const BulletinPage = () => {
+  return <>This is a BulletinPage</>
+}
+
+export default BulletinPage
