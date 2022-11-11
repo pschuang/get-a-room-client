829a4860f2a4a38c170840e2ca98b2c71a5f8d97
Pin Shan Chuang <pshanchuang@gmail.com>
2022-11-11 11:16:39 +0800

    fix: 上傳註冊用頭貼圖片 & 修改 SignUpBox component 圖片存取位置

--- src/pages/SignUpPage/SignUpBox/SignUpBox.js
+++ src/pages/SignUpPage/SignUpBox/SignUpBox.js
@@ -5,7 +5,7 @@ const SignUpBox = () => {
     <div className="signup-main-container">
       <div className="signup-left-half">
         <div className="signup-icon-bar">
-          <img src="" alt="" />
+          <img src="ostrich.png" alt="logo" />
           <p>GET A ROOM</p>
         </div>
         <form className="signup-form">
@@ -17,53 +17,28 @@ const SignUpBox = () => {
           <input id="nickname"></input>
           <label for="nickname">choose a picture</label>
           <div className="image-options-list">
-            <img
-              src="https://cdn-icons-png.flaticon.com/512/3940/3940417.png"
-              alt=""
-            />
-            <img
-              src="https://cdn-icons-png.flaticon.com/512/3940/3940403.png"
-              alt=""
-            />
-            <img
-              src="https://cdn-icons-png.flaticon.com/512/3940/3940415.png"
-              alt=""
-            />
-            <img
-              src="https://cdn-icons-png.flaticon.com/512/3940/3940410.png"
-              alt=""
-            />
-            <img
-              src="https://cdn-icons-png.flaticon.com/512/3940/3940416.png"
-              alt=""
-            />
-            <img
-              src="https://cdn-icons-png.flaticon.com/512/3940/3940419.png"
-              alt=""
-            />
-            <img
-              src="https://cdn-icons-png.flaticon.com/512/3940/3940401.png"
-              alt=""
-            />
-            <img
-              src="https://cdn-icons-png.flaticon.com/512/3940/3940418.png"
-              alt=""
-            />
-            <img
-              src="https://cdn-icons-png.flaticon.com/512/3940/3940405.png"
-              alt=""
-            />
-            <img
-              src="https://cdn-icons-png.flaticon.com/512/3940/3940419.png"
-              alt=""
-            />
+            <img src="rabbit.png" alt="bear" />
+            <img src="puffer-fish.png" alt="" />
+            <img src="bear.png" alt="" />
+            <img src="deer.png" alt="" />
+            <img src="dog.png" alt="" />
+            <img src="elephant.png" alt="" />
+            <img src="lion.png" alt="" />
+            <img src="llama.png" alt="" />
+            <img src="ostrich.png" alt="" />
+            <img src="panda.png" alt="" />
           </div>
         </form>
         <button className="signup-button">Sign up</button>
         <span>OR</span>
         <button className="login-button">Log in</button>
       </div>
-      <div className="signup-right-half"><img src="https://images.unsplash.com/photo-1606245081924-c08c2c463d2b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=875&q=80" alt="" /></div>
+      <div className="signup-right-half">
+        <img
+          src="https://images.unsplash.com/photo-1606245081924-c08c2c463d2b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=875&q=80"
+          alt=""
+        />
+      </div>
     </div>
   )
 }
