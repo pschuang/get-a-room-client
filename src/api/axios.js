dbba30e3595c1b3d29a7514de3390acf5cd4cbc6
Pin Shan Chuang <pshanchuang@gmail.com>
2022-11-12 23:40:53 +0800

    chores: axios API jwt headers 設定

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,14 @@
 import axios from 'axios'
 
-const instance = axios.create({ baseURL: 'http://localhost:8000/api/1.0' })
+const instance = axios.create({
+  baseURL: 'http://localhost:8000/api/1.0',
+})
+
+instance.interceptors.request.use(function (config) {
+  // Do something before request is sent
+  let token = localStorage.getItem('access_token')
+  config.headers['Authorization'] = 'Bearer ' + token
+  return config
+})
 
 export default instance
