cc8c0510de601b7d1b39fcc3799b60219b6df169
Pin Shan Chuang <pshanchuang@gmail.com>
2022-11-12 13:30:43 +0800

    feat: 串接 GET / questions API

--- src/pages/BulletinPage/BulletinPage.js
+++ src/pages/BulletinPage/BulletinPage.js
@@ -1,8 +1,60 @@
 import './BulletinPage.css'
 import QuestionInput from './QuestionInput/QuestionInput'
 import QuestionUnit from './QuestionUnit/QuestionUnit'
+import { useState, useEffect } from 'react'
+import { useSearchParams, useLocation } from 'react-router-dom'
+import axios from '../../api/axios'
+
+const categoryList = [
+  { color: '#333333', name: 'all' },
+  { color: '#FFBDD9', name: 'sport' },
+  { color: '#E5C0FF', name: 'food' },
+  { color: '#92F8FF', name: 'philosophy' },
+  { color: 'red', name: 'politics' },
+  { color: 'coral', name: 'health' },
+  { color: '#92FF88', name: 'movie' },
+  { color: 'blue', name: 'leisure' },
+]
 
 const BulletinPage = ({ userId }) => {
+  const [keyword, setKeyord] = useState()
+  const [questions, setQuestions] = useState([])
+  let [searchParams, setSearchParams] = useSearchParams()
+  let location = useLocation()
+
+  const handleCategoryClick = (category) => {
+    searchParams.set('category', category)
+    setSearchParams(searchParams)
+  }
+
+  const handleSearchClick = () => {
+    searchParams.set('keyword', keyword)
+    setSearchParams(searchParams)
+  }
+
+  const handleClearFilter = () => {
+    searchParams.set('category', 'all')
+    setSearchParams()
+  }
+
+  const getQuestions = async () => {
+    const category = searchParams.get('category')
+    const keyword = searchParams.get('keyword')
+    const response = await axios({
+      method: 'GET',
+      url: `/questions/${category}`,
+      params: {
+        keyword: keyword,
+      },
+    })
+    setQuestions(response.data.questions)
+  }
+  useEffect(() => {
+    console.log(location)
+    getQuestions()
+  }, [location])
+
+
   return (
     <>
       <h1>This is a BulletinPage</h1>
@@ -12,24 +64,39 @@ const BulletinPage = ({ userId }) => {
           <QuestionInput />
           <div className="category-search-container">
             <div className="category-list">
-              <div className="category">#movie</div>
-              <div className="category">#sport</div>
-              <div className="category">#travel</div>
-              <div className="category">#foodie</div>
-              <div className="category">#foodie</div>
-              <div className="category">#foodie</div>
-              <div className="category">#foodie</div>
-              <div className="category">#foodie</div>
-              <div className="category">#foodie</div>
+              {categoryList.map((category, index) => (
+                <div
+                  key={index}
+                  className="category"
+                  onClick={() => handleCategoryClick(category.name)}
+                >
+                  <span style={{ color: category.color }}>#</span>
+                  {category.name}
+                </div>
+              ))}
             </div>
-            <input className="search" placeholder="search" />
+            <button onClick={handleClearFilter}>Clear</button>
+            <input
+              className="search"
+              placeholder="search"
+              value={keyword}
+              onChange={(e) => {
+                setKeyord(e.target.value)
+              }}
+            />
+            <button className="search-button" onClick={handleSearchClick}>
+              SEARCH
+            </button>
           </div>
           <div className="temporary-question-list">
-            <QuestionUnit />
-            <QuestionUnit />
-            <QuestionUnit />
-            <QuestionUnit />
-            <QuestionUnit />
+            {questions.map((question) => {
+              return (
+                <QuestionUnit
+                  question={question}
+                  key={`question-${question.id}`}
+                />
+              )
+            })}
           </div>
         </div>
       </div>
--- src/pages/BulletinPage/QuestionUnit/QuestionUnit.js
+++ src/pages/BulletinPage/QuestionUnit/QuestionUnit.js
@@ -1,27 +1,22 @@
 import './QuestionUnit.css'
-const QuestionUnit = () => {
+const QuestionUnit = ({ question }) => {
   return (
     <div className="question-unit">
       <div className="category-bar">
-        <div className="category-tag">#movie</div>
+        <div className="category-tag">#{question.category}</div>
       </div>
       <div className="question-box">
         <div className="question-box-name">
-          <img
-            src="https://cdn-icons-png.flaticon.com/512/3940/3940437.png"
-            alt=""
-          />
-          <div style={{ margin: '10px' }}>name</div>
+          <img src={question.pictureURL} alt="" />
+          <div style={{ margin: '10px' }}>{question.nickname}</div>
         </div>
-        <div className="question-box-question">
-          QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion
-        </div>
+        <div className="question-box-question">{question.content}</div>
         <div className="question-box-bottom">
           <img
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9G5MDo5FXZmbb_ixkP93imuP5N4KMAGkGlw&usqp=CAU"
             alt=""
           />
-          <div>33</div>
+          <div>{question.reply_counts}</div>
           <div className="button-div">
             <button>Reply</button>
           </div>
