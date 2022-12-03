241432741bdb50d1571d206f9389a2e566673f96
Pin Shan Chuang <pshanchuang@gmail.com>
2022-12-03 21:08:22 +0800

    feat: 布告欄新增使用規則 & 目前搜尋、篩選狀態條

--- src/pages/BulletinPage/BulletinPage.js
+++ src/pages/BulletinPage/BulletinPage.js
@@ -34,6 +34,8 @@ const BulletinPage = ({ socket }) => {
   const [closedTime, setClosedTime] = useState('')
   const [nextOpenTime, setNextOpenTime] = useState('')
   const [picture, setPicture] = useState('')
+  const [currentFilterCategory, setCurrentFilterCategory] = useState('')
+  const [currentKeyword, setCurrentKeyword] = useState('')
 
   let [searchParams, setSearchParams] = useSearchParams()
   let location = useLocation()
@@ -41,22 +43,27 @@ const BulletinPage = ({ socket }) => {
   const handleCategoryClick = (category) => {
     searchParams.set('category', category)
     setSearchParams(searchParams)
+    setCurrentFilterCategory(category)
   }
 
   const handleSearchClick = () => {
     searchParams.set('keyword', keyword)
     setSearchParams(searchParams)
+    setCurrentKeyword(keyword)
+    setKeyord('')
   }
 
   const handleClearFilter = () => {
     searchParams.set('category', 'all')
     setSearchParams()
+    setCurrentFilterCategory('')
+    setCurrentKeyword('')
   }
 
   const handleRuleClick = () => {
     Swal.fire({
       title: 'Get a room rules',
-      html: '<div style="margin-left:60px"><p style="line-height:120%" align="justify">1. Bulletin open for 20 mins, once a day <br> 2. You can post 1 question, replies not limited <br> 3. Once matched, you can chat for 15 mins <br> 4. When match ends, decide to be friends in 30s</p></div>',
+      html: `<div style="margin-left:30px"><p style="line-height:120%" align="justify">1.  Bulletin open for 20 mins, once a day <br>2. You can post 1 question, and create multiple replies. <br>3. Pick a reply or wait to be picked to create a chatroom. <br>4. Once matched, you can chat for 15 mins <br>5. When match ends, decide to be friends in 30s</p></div>`,
     })
   }
 
@@ -121,15 +128,23 @@ const BulletinPage = ({ socket }) => {
     getQuestions()
   }
 
+  useEffect(() => {
+    if (!window.localStorage.getItem('has-shown-rule')) {
+      handleRuleClick()
+      window.localStorage.setItem('has-shown-rule', true)
+    }
+  }, [])
+
   return (
     <>
       <div className="main-page-container">
         <Header socket={socket} />
-        <FriendList />
+        <FriendList socket={socket} />
         <div className="page-right-container">
           <div className="main-content-container">
             <button className="rule-banner" onClick={handleRuleClick}>
-              Check playground rule
+              <img src="/problems.png" alt="" />
+              <p>Check playground rule</p>
             </button>
             {isBulletinClosed ? (
               <div className="closed-container">
@@ -142,7 +157,12 @@ const BulletinPage = ({ socket }) => {
               </div>
             ) : (
               <div className="bulletin-main-container">
-                <QuestionInput picture={picture} setPicture={setPicture} />
+                <QuestionInput
+                  picture={picture}
+                  setPicture={setPicture}
+                  getQuestions={getQuestions}
+                  socket={socket}
+                />
                 <div className="category-search-container">
                   <div>
                     <div className="category-list">
@@ -157,22 +177,33 @@ const BulletinPage = ({ socket }) => {
                         </div>
                       ))}
                     </div>
-                    <button
-                      className="clear-button"
-                      onClick={handleClearFilter}
-                    >
-                      Clear
-                    </button>
-                    <button
-                      onClick={handleRefreshQuestions}
-                      className="clear-button refresh-button"
-                    >
-                      Refresh
-                    </button>
+                    <div className="filter-keyword-status-container">
+                      <button
+                        className="clear-button"
+                        onClick={handleClearFilter}
+                      >
+                        Clear
+                      </button>
+                      <button
+                        onClick={handleRefreshQuestions}
+                        className="clear-button refresh-button"
+                      >
+                        Refresh
+                      </button>
+                      {(currentFilterCategory || currentKeyword) && (
+                        <div className="filter-keyword-status">
+                          <p>Now Searching: </p>
+                          {currentFilterCategory && (
+                            <div># {currentFilterCategory}</div>
+                          )}
+                          {currentKeyword && <div>{currentKeyword}</div>}
+                        </div>
+                      )}
+                    </div>
                   </div>
                   <div className="search">
                     <input
-                      placeholder="search"
+                      placeholder="type keyword here"
                       value={keyword}
                       onChange={(e) => {
                         setKeyord(e.target.value)
@@ -218,6 +249,8 @@ const BulletinPage = ({ socket }) => {
           question={selectedQuestion}
           onClose={() => setIsShowReplyPopUp(false)}
           picture={picture}
+          getQuestions={getQuestions}
+          socket={socket}
         />
       )}
     </>
